5df550ffa2ebf2d030ee4dd4be0b962318d1b9f5
Ivan Tsekhmistro <itsekhmistro@adyax.com>
2019-11-20 11:14:38 +0200

    Add Docker makefile. Fix Mysql.conf (switch to MySQL 5.7).

---
+++ Makefile
@@ -0,0 +1,6 @@
+include docker.mk
+
+.PHONY: test
+
+ORO_APP ?= /Users/itsekhmistro/data/www/oro.m1/orodev/application
+SYMFONY_ENV ?= SYMFONY_ENV
---
+++ docker.mk
@@ -0,0 +1,48 @@
+include .env
+
+.PHONY: up down stop prune ps shell drush logs
+
+default: up
+
+# ORO_APP ?= /Users/itsekhmistro/data/www/oro.m1/orodev/application
+# SYMFONY_ENV ?= prod
+export ORO_APP=/Users/itsekhmistro/data/www/oro.m1/orodev/application
+export SYMFONY_ENV=prod
+
+up:
+	@echo "Starting up containers for $(PROJECT_NAME)..."
+# 	docker-compose pull --parallel
+	docker-compose up -d --remove-orphans
+
+down: stop
+
+stop:
+	@echo "Stopping containers for $(PROJECT_NAME)..."
+	docker-compose stop
+
+prune:
+	@echo "Removing containers for $(PROJECT_NAME)..."
+	docker-compose down -v
+
+ps:
+	@docker ps --filter name='$(PROJECT_NAME)*'
+
+shell:
+	docker exec -ti -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell docker ps --filter name='php' --format "{{ .ID }}") /bin/bash
+
+
+mys:
+	docker exec -ti -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell docker ps --filter name='database' --format "{{ .ID }}") /bin/bash
+
+# shellnginx:
+# 	docker exec -ti $(shell docker ps --filter name='$(PROJECT_NAME)_nginx' --format "{{ .ID }}") sh
+
+# drush:
+# 	docker exec $(shell docker ps --filter name='$(PROJECT_NAME)_php' --format "{{ .ID }}") /bin/bash -c "export SUDO_USER=wodby && bin/drush -r $(DRUPAL_ROOT) $(filter-out $@,$(MAKECMDGOALS))"
+
+logs:
+	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
+
+# https://stackoverflow.com/a/6273809/1826109
+%:
+	@:
